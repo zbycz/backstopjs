6a7f361d06a4108e78eb94f3048b20a8580adaa6
Pavel Zbytovsky <pavel.zbytovsky@gooddata.com>
2018-01-12 15:46:35 +0100

    ignoreAntialiasing

--- core/util/compare/compare-resemble.js
+++ core/util/compare/compare-resemble.js
@@ -3,7 +3,7 @@ var resemble = require('node-resemble-js');
 module.exports = function (referencePath, testPath, misMatchThreshold, resembleOutputSettings, requireSameDimensions) {
   return new Promise(function (resolve, reject) {
     resemble.outputSettings(resembleOutputSettings || {});
-    resemble(referencePath).compareTo(testPath).onComplete(data => {
+    resemble(referencePath).compareTo(testPath).ignoreAntialiasing().onComplete(data => {
       if ((requireSameDimensions === false || data.isSameDimensions === true) && data.misMatchPercentage <= misMatchThreshold) {
         return resolve(data);
       }
